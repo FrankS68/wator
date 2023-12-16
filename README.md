73342612b776280f2879efd772292e936b6d59ea
frank <frank@H-A-L-2023.fritz.box>
2023-12-16 21:33:18 +0100

    restructured

--- README.md
+++
@@ -1,5 +0,0 @@
-wator
-=====
-
-mainly just a test to know about git(hub) <br/>
-but also trying to implement something like http://wikipedia.org/wiki/Wator
---
+++ src/main/java/de/witchcafe/wator/WatorApplication.java
@@ -0,0 +1,13 @@
+package de.witchcafe.wator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatorApplication.class, args);
+	}
+
+}
